0d99c39a5fb2d842154c77ce89f4faf1657be3e8
Gladys2212 <dsm23190102.gluis@alumnos.utsv.edu.mx>
2024-08-16 09:57:45 -0600

    Delete intro-html-gla directory

--- intro-html-gla/js/DarkMode.js
+++
@@ -1,7 +0,0 @@
-const activeDarkMode = () => {
-  const text = document.getElementById("text");
-  const body = document.getElementById("body");
-  body.classList.toggle("active");
-
-  text.textContent = text.textContent === "Nocturno" ? "Claro" : "Nocturno";
-};
