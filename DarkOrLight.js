f6c0b3719e977c0c547b9982b543a07b4aa4c987
Gladys2212 <dsm23190102.gluis@alumnos.utsv.edu.mx>
2024-08-16 10:02:27 -0600

    Delete DarkOrLight.js

--- DarkOrLight.js
+++
@@ -1,9 +0,0 @@
-const activeDarkMode = () => {
-  const body = document.getElementById("body");
-  const title = document.getElementById("title");
-  const btn = document.getElementById("btn");
-
-  body.classList.toggle("active");
-  title.textContent = "Modo Claro";
-  btn.textContent = "desactivar";
-};
